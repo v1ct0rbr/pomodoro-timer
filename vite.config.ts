0e678ed1abc9eff4dc8bf0959580d9ea4f01e8ae
queirogavictor-at-892414404840 <victorqueiroga.jp@gmail.com>
2024-11-27 14:14:28 -0300

    feat: add install script and configure Vite server settings

--- vite.config.ts
+++ vite.config.ts
@@ -4,4 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 3000, // Set a custom port
+    host: '0.0.0.0', // Bind to all available network interfaces
+    open: true, // Open the browser automatically when the server starts
+  },
+  preview: {
+    port: 3000,
+  },
 })
